f9f0750a30e449f06b3e45726511443083525b6f
SteveViss <steve.vissault@insileco.io>
2024-02-15 14:15:15 -0500

    CST-261 Fix data documentation

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Year-round Tracking of Breeding Gulls in Nunatsiavut
+#'
+#' This dataset represents all locations of the movebank project ID 2854587542
+#' This static extraction has been made February, 15th, 2024
+#'
+#' @format 
+#' A data frame with 40,477 rows and 40 columns
+"nunatsiavut_gulls"
