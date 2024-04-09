23a6bf3c33fc0b7583d275faff78f0d98d390bff
SteveViss <steve.vissault@insileco.io>
2024-04-09 18:14:00 -0400

    Fix dockerfile and app deployment

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-Rscript -e "shiny::runApp(port = 5000, host = '0.0.0.0')" 2>&1
+# Mettre les commmandes sops pour decrypt les secrets via docker env
+Rscript -e "nunatsiavutBirdTracker::run_app(options = list(port = 5000, host = "0.0.0.0"))" 2>&1
