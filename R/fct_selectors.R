beaeb13756d7a20975a25f0156f6affec42b923b
SteveViss <steve.vissault@insileco.io>
2024-04-30 19:00:40 -0400

    CST-351 add year filter

--- R/fct_selectors.R
+++ R/fct_selectors.R
@@ -2,16 +2,30 @@
 #'
 #' @description A function to select a map.
 #'
-#' @param geoms a sf object.
-#' @param layer a specific layer.
-#' @param set_view a logical. Should the view be (re)set.
+#' @param ds an arrow::gs_bucket object.
 #'
 #' @return The return value, if any, from executing the function.
 #'
 #' @noRd
 
 fetch_input_choices <- function(ds = NULL){
-    ds |> dplyr::group_by(tag_id, vernacular, species) |>
-        dplyr::summarize(min = min(datetime), max = max(datetime)) |>
-        dplyr::collect()
+    data <- ds |> dplyr::collect()
+
+    loc_years <- data |>
+        dplyr::mutate(year = lubridate::year(datetime)) |>
+        dplyr::select(tag_id, year) |>
+        dplyr::distinct()
+
+    loc_summary <- data |>
+        dplyr::group_by(tag_id, vernacular, species) |>
+        dplyr::mutate(year = lubridate::year(datetime)) |>
+        dplyr::summarize(min = min(datetime), max = max(datetime))
+    
+    loc_summary |>
+        dplyr::mutate(years = purrr::map(tag_id, \(t) {
+            loc_years |>
+                dplyr::filter(tag_id == t) |>
+                dplyr::pull(year) |>
+                unique() 
+        }))
 }
