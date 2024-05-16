9c832c0842ade13371912842f6b9d4621642c80b
SteveViss <steve.vissault@insileco.io>
2024-05-16 15:22:05 -0400

    CST-296 Init bird description from database

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,26 +10,29 @@ app_ui <- function(request) {
     # Leave this function for adding external resources
     golem_add_external_resources()
     bslib::page_sidebar(
+        title = "Nunatsiavut bird trackers",
         class = "p-0",
         theme = bslib::bs_theme(
             base_font = bslib::font_google("Poppins"),
             "bslib-dashboard-design" = "false"
         ),
         sidebar = bslib::sidebar(
-            title = "Nunatsiavut bird trackers",
             position = "left",
             width = "25%",
-            mod_selectors_ui("selectors"),
+            mod_selectors_ui("selectors")
+        ),
+        bslib::layout_column_wrap(
             bslib::card(
                 bslib::card_body(
-                    bslib::card_title("Michelle"),
-                    img(src="www/img/201642.png", class="img-fluid", alt="Test"),
-                    bslib::card_title("Description"),
-                    lorem::ipsum(paragraphs = 1)
+                    class = "p-0",
+                    full_screen = TRUE,
+                    mod_map_ui("map")
                 )
-            )
-        ),
-        mod_map_ui("map")
+            ),
+            mod_bird_description_ui("bird_description"),
+            width = NULL,
+            style = htmltools::css(grid_template_columns = "1.25fr 0.75fr")
+        )
     )
 }
 
