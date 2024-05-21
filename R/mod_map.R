016ef07d20a49d92ef2a07b14992001075f3d494
SteveViss <steve.vissault@insileco.io>
2024-05-21 15:38:31 -0400

    Remove year and add GPS location marker

--- R/mod_map.R
+++ R/mod_map.R
@@ -22,39 +22,45 @@ mod_map_server <- function(id, r) {
 
         observeEvent(r$selectedBirdDescription, {
             req(r$selectedBirdDescription)
+            cli::cli_alert_info("Map - Set bird GPS deployment")
+
+            labelText <- paste0("<b>GPS deployment location</b><br/>",
+                                "Date: ", r$selectedBirdDescription$tag_deployment_date)
 
             leaflet::leafletProxy("map-map", session) |>
-                leaflet:: addCircleMarkers(
+                leaflet::addCircleMarkers(
+                    label = htmltools::HTML(labelText),
                     lng = r$selectedBirdDescription$longitude,
                     lat = r$selectedBirdDescription$latitude,
                     color = "#BF2C34",
                     fill = TRUE,
-                    radius = 3,
-                    fillOpacity = 1
+                    radius = 10,
+                    stroke = FALSE,
+                    fillOpacity = 1,
+                    labelOptions = leaflet::labelOptions(noHide = TRUE)
                 )
 
         })
 
-        observeEvent({
-                r$tag_id 
-                r$year 
-            }, {
-            req(r$tag_id, r$year)
-            cli::cli_alert_info("Map - Fetch spatial informations for {r$tag_id} and year {r$year}")
-            stmp <- fetch_spatial_ind(ds = r$arrow_dataset, ind = r$tag_id, year = r$year)
+        observeEvent(r$tag_id, {
+            req(r$tag_id)
+            cli::cli_alert_info("Map - Fetch spatial informations for {r$tag_id}")
+            
+            stmp <- fetch_spatial_ind(ds = r$arrow_dataset, ind = r$tag_id)
             bbox <- sf::st_bbox(stmp$lines) |> as.vector()
-            color_line <- "#FFCC00" 
+            color_line <- "#FFCC00"
+            
             leaflet::leafletProxy("map-map", session) |>
                 leaflet::clearMarkers() |>
                 leaflet::clearShapes() |>
                 leaflet.extras2::addArrowhead(
                     data = stmp$lines,
                     color = color_line,
-                    opacity = 0.8,
+                    opacity = 0.6,
                     options = leaflet.extras2::arrowheadOptions(
                         color = color_line,
                         size = "10px",
-                        frequency = "100px",
+                        frequency = "300px",
                         yawn = 60,
                         fill = TRUE,
                         proportionalToTotal = FALSE
