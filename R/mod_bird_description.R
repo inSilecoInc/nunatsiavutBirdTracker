9a1b28174826b58bedc451e3e7692eaa29bbe445
SteveViss <steve.vissault@insileco.io>
2024-05-22 19:28:19 -0400

    CST-253 Review Kev

--- R/mod_bird_description.R
+++ R/mod_bird_description.R
@@ -1,12 +1,11 @@
 #' bird_description UI Function
 #'
-#' @description A shiny Module.
+#' @description This module displays various informations on the individual bird selected
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
 #' @noRd 
 #'
-#' @importFrom shiny NS tagList 
 mod_bird_description_ui <- function(id){
   ns <- NS(id)
   uiOutput(ns("description"))
@@ -18,13 +17,6 @@ mod_bird_description_ui <- function(id){
 mod_bird_description_server <- function(id, r){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
-
-    googleCloudStorageR::gcs_auth(Sys.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-    birds_metadata <- googleCloudStorageR::gcs_get_object("bird-metadata.rds",
-      bucket = "bird-metadata", 
-      parseFunction= googleCloudStorageR::gcs_parse_rds
-    )
-
     observeEvent(r$tag_id, {
       req(r$tag_id, birds_metadata)
       selDesc <- dplyr::filter(birds_metadata, tag_id == r$tag_id)
