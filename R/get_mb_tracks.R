0572bb1094a030a4e35e971c6d8b20d4abf74195
SteveViss <steve.vissault@insileco.io>
2024-07-03 11:59:20 -0400

    Ini sync bucket procedure

---
+++ R/get_mb_tracks.R
@@ -0,0 +1,59 @@
+#' Retrieve movebank tracks
+#'
+#' @description Retrieve tracks from movebank project Id. 
+#'
+#' @param mb_user movebank username.
+#' @param mb_password movebank password.
+#' @param study_id movebank studyId.
+#'
+#' @return Return a data.frame of 7 columns:
+#' - tag_id: Individual tag id
+#' - band_id: Individual band id
+#' - lon: Longitude of the location
+#' - lat: Latitude of the location
+#' - datetime: Datetime of the location
+#' - species: Species latin name
+#' - vernacular: Species english name
+#'
+
+get_mb_tracks <- function(mb_user=NULL, mb_password=NULL, study_id=2854587542){
+
+    suppressWarnings({
+        suppressMessages({
+            loginStored <- move::movebankLogin(username = mb_user, password = mb_password)
+
+            tracks <- move::getMovebankData(study = study_id, login = loginStored, removeDuplicatedTimestamps = TRUE, deploymentAsIndividuals = TRUE)@data
+
+            animals <- move::getMovebankAnimals(study = study_id, login = loginStored) |>
+                dplyr::select(tag_id, local_identifier, tag_local_identifier, sensor_type_id, taxon_canonical_name)
+
+            tracks_df <- tracks |>
+                dplyr::left_join(animals)
+        })
+    })
+
+    tracks_df <- tracks_df |>
+        janitor::remove_empty(c("rows", "cols")) |>
+        janitor::remove_constant() |>
+        dplyr::filter(sensor_type == "GPS") |>
+        dplyr::select(
+            tag_id = tag_local_identifier,
+            band_id = local_identifier,
+            lon = location_long,
+            lat = location_lat,
+            datetime = timestamp,
+            species = taxon_canonical_name
+        ) |>
+        dplyr::mutate(
+            vernacular = dplyr::case_when(
+                species == "Larus marinus" ~ "Great Black-backed Gull",
+                species == "Larus hyperboreus" ~ "Glaucous Gull",
+                species == "Larus argentatus" ~ "Herring Gull",
+                TRUE ~ "Undefined"
+            ),
+            tag_id = as.factor(tag_id),
+            datetime = as.POSIXct(datetime)
+        )
+    
+    return(tracks_df)
+}
---
+++ R/sync_gs_parquet.R
@@ -0,0 +1,31 @@
+#' Write parquet tracks in GCS bucket 
+#'
+#' @description Write parquet files with animals tracks into the target GCS bucket. 
+#'
+#' @param bucket GCS bucket name.
+#' @param auth_gcs_file_path movebank password.
+#' @param ... arguments passed to get_mb_tracks function 
+#'
+sync_gs_parquet <- function(bucket=NULL, auth_gcs_file_path="./nunatsiavut-birds-f33436183827.json", ...){
+
+  temp_parquet_gulls <- tempfile()
+
+  arrow::write_dataset(
+    get_mb_tracks(...),
+    temp_parquet_gulls,
+    basename_template = "part-{i}.parquet",
+    format = "parquet",
+    partitioning = c("vernacular", "tag_id"),
+    existing_data_behavior = "overwrite"
+  )
+
+  googleCloudStorageR::gcs_auth(auth_gcs_file_path)
+
+  parquet_files <- list.files(temp_parquet_gulls, recursive = TRUE)
+
+  parquet_files |> purrr::walk(\(f) {
+    googleCloudStorageR::gcs_upload(file.path(temp_parquet_gulls, f), name = f, bucket = bucket, predefinedAcl = "bucketLevel")
+  })
+
+  on.exit(file.remove(temp_parquet_gulls))
+}
