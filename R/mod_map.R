8ae50e00f1166c12888465d5cf85b35887bc0e73
Kevin Cazelles <kevin.cazelles@insileco.io>
2024-12-16 12:28:00 -0500

    CST-434 origin point on top

--- R/mod_map.R
+++ R/mod_map.R
@@ -20,69 +20,97 @@ mod_map_server <- function(id, r) {
 
         output$map <- leaflet::renderLeaflet(base_map())
 
-        observeEvent(r$selectedBirdDescription, {
-            req(r$selectedBirdDescription)
-            cli::cli_alert_info("Map - Set bird GPS deployment")
+        labelText <- reactive({
+            paste0(
+                "<b>GPS deployment location</b><br/>",
+                "Date: ", r$selectedBirdDescription$tag_deployment_date
+            )
+        })
 
-            labelText <- paste0("<b>GPS deployment location</b><br/>",
-                                "Date: ", r$selectedBirdDescription$tag_deployment_date)
-
-            leaflet::leafletProxy("map-map", session) |>
-                leaflet::addCircleMarkers(
-                    label = htmltools::HTML(labelText),
-                    lng = r$selectedBirdDescription$longitude,
-                    lat = r$selectedBirdDescription$latitude,
-                    color = "#BF2C34",
-                    fill = TRUE,
-                    radius = 10,
-                    stroke = FALSE,
-                    fillOpacity = 1,
-                    labelOptions = leaflet::labelOptions(noHide = TRUE)
-                )
-
+        spat_line <- reactive({
+            cli::cli_alert_info("Map - Fetch spatial informations for {isolate(r$tag_id)}")
+            fetch_spatial_ind(
+                ds = r$arrow_dataset,
+                ind = r$tag_id,
+                max_date = r$max_date
+            )
         })
 
         observe({
-            req(r$tag_id)
-            cli::cli_alert_info("Map - Fetch spatial informations for {r$tag_id}")
-            
-            stmp <- fetch_spatial_ind(
-                ds = r$arrow_dataset, ind = r$tag_id, max_date = r$max_date
-            )
-            bbox <- sf::st_bbox(stmp$lines) |> 
-                as.vector()
-            color_line <- "#FFCC00"
-            
-            leaflet::leafletProxy("map-map", session) |>
-                leaflet::clearMarkers() |>
-                leaflet::clearShapes() |>
-                leaflet.extras2::addArrowhead(
-                    data = stmp$lines,
-                    color = color_line,
-                    opacity = 0.6,
-                    options = leaflet.extras2::arrowheadOptions(
-                        color = color_line,
-                        size = "10px",
-                        frequency = "300px",
-                        yawn = 60,
-                        fill = TRUE,
-                        proportionalToTotal = FALSE
-                    )
-                ) |>
-                leaflet::addCircleMarkers(
-                    data = stmp$points,
-                    label = ~datetime,
-                    color = color_line,
-                    radius = 3,
-                    stroke = FALSE,
-                    fillOpacity = 1
-                ) |>
-                leaflet::flyToBounds(
-                    lng1 = bbox[1],
-                    lat1 = bbox[2],
-                    lng2 = bbox[3],
-                    lat2 = bbox[4]
-                )
+            if (!is.null(r$selectedBirdDescription)) {
+                stmp <- spat_line()
+                if (nrow(stmp$lines)) {
+                    bbox <- sf::st_bbox(stmp$lines) |>
+                        as.vector()
+                    color_line <- "#FFCC00"
+                    leaflet::leafletProxy("map-map", session) |>
+                        leaflet::clearMarkers() |>
+                        leaflet::clearShapes() |>
+                        leaflet::addMapPane("main", zIndex = 410) |>
+                        leaflet::addMapPane("overlay", zIndex = 420) |>
+                        leaflet.extras2::addArrowhead(
+                            data = stmp$lines,
+                            color = color_line,
+                            opacity = 0.6,
+                            options = c(
+                                leaflet.extras2::arrowheadOptions(
+                                    color = color_line,
+                                    size = "10px",
+                                    frequency = "300px",
+                                    yawn = 60,
+                                    fill = TRUE,
+                                    proportionalToTotal = FALSE
+                                ),
+                                leaflet::pathOptions(pane = "main")
+                            )
+                        ) |>
+                        leaflet::addCircleMarkers(
+                            data = stmp$points,
+                            label = ~datetime,
+                            color = color_line,
+                            radius = 3,
+                            stroke = FALSE,
+                            fillOpacity = 1,
+                            options = leaflet::pathOptions(pane = "main")
+                        ) |>
+                        create_origin_marker(
+                            isolate(r$selectedBirdDescription$longitude),
+                            isolate(r$selectedBirdDescription$latitude),
+                            labelText()
+                        ) |>
+                        leaflet::flyToBounds(
+                            lng1 = bbox[1],
+                            lat1 = bbox[2],
+                            lng2 = bbox[3],
+                            lat2 = bbox[4]
+                        )
+                } else {
+                    cli::cli_alert_info("Map - Set bird GPS deployment")
+                    leaflet::leafletProxy("map-map", session) |>
+                        create_origin_marker(
+                            r$selectedBirdDescription$longitude,
+                            r$selectedBirdDescription$latitude,
+                            labelText()
+                        )
+                }
+            }
         })
     })
+}
+
+
+create_origin_marker <- function(map, lng, lat, lab, no_hide = FALSE) {
+    leaflet::addCircleMarkers(
+        map,
+        label = htmltools::HTML(lab),
+        lng = lng,
+        lat = lat,
+        color = "#BF2C34",
+        fill = TRUE,
+        radius = 10,
+        stroke = FALSE,
+        fillOpacity = 1,
+        labelOptions = leaflet::labelOptions(noHide = no_hide),
+        options = leaflet::pathOptions(pane = "overlay")
+    )
 }
